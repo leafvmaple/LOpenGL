2c9e26016e8756b255ca57956fc597a5d6c09bb9
LeafMaple <leafvmaple@163.com>
2018-06-19 22:35:55 +0900

    Add GLFW & GLAD.

--- LOpenGL/src/LGLMain.cpp
+++ LOpenGL/src/LGLMain.cpp
@@ -6,4 +6,62 @@
 //  Copyright © 2018 LeafMaple. All rights reserved.
 //
 
-#include "LGLMain.hpp"
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+void FrameBufferSizeFunc(GLFWwindow* pWindow, int nWidth, int nHeight)
+{
+    glViewport(0, 0, nWidth, nHeight);
+}
+
+void ProcessInput(GLFWwindow *pWindow)
+{
+    if(glfwGetKey(pWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(pWindow, true);
+}
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+    GLFWwindow* pWindow = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LOpenGL", NULL, NULL);
+    if (pWindow == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(pWindow);
+    glfwSetFramebufferSizeCallback(pWindow, FrameBufferSizeFunc);
+    
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+    
+    
+    while(!glfwWindowShouldClose(pWindow))
+    {
+        ProcessInput(pWindow);
+        
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        glfwSwapBuffers(pWindow);
+        glfwPollEvents();
+    }
+    
+    glfwTerminate();
+    return 0;
+}
