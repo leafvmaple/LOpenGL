d7fd82f9f06a6de70e79e2d17cc02c878cc99bae
LeafMaple <leafvmaple@163.com>
2018-07-07 13:32:33 +0900

    Add MeshCreator.

---
+++ LCreator/src/LMeshCreator.cpp
@@ -0,0 +1,9 @@
+//
+//  LMeshCreator.cpp
+//  LCreator
+//
+//  Created by LeafMaple on 2018/07/07.
+//  Copyright © 2018 LeafMaple. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ LCreator/src/LMeshCreator.h
@@ -0,0 +1,13 @@
+//
+//  LMeshCreator.h
+//  LCreator
+//
+//  Created by LeafMaple on 2018/07/07.
+//  Copyright © 2018 LeafMaple. All rights reserved.
+//
+
+#ifndef LMeshCreator_h
+#define LMeshCreator_h
+
+
+#endif /* LMeshCreator_h */
